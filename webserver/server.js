fc49aebe0180986778288e11733fb89b35b4bb4f
Tedros <tedtfu@gmail.com>
2020-05-12 17:26:40 -0400

    port changed

--- webserver/server.js
+++ webserver/server.js
@@ -1,10 +1,8 @@
 const express=require('express');
 const {connectToDB,insertOrders,register,login,ordersList}=require('./Database.js')
-
 const cors=require('cors');
-const port=process.env.PORT||process.env.PORT_SECONDARY;
-const app=express();
 
+const app=express();
 app.use(cors());
 // app.use('/api',(req,res,next)=>router(req,res,next));
 
@@ -12,9 +10,9 @@ app.use(cors());
     await connectToDB(process.env.DB_URL2);
 
 })();
-// app.get('/',(req,res)=>{
-//     res.send("hello world")
-// })
+app.get('/',(req,res)=>{
+    res.send("hello world")
+})
 app.post('/api/orders',(req,res)=>{
     insertOrders(req.body)
 })
@@ -37,5 +35,5 @@ app.post("/api/login",async (req,res)=>{
 //     createServer();
 //     console.log("Reloading web servcie data complete")
 // });
-
+const port=process.env.PORT||process.env.PORT_SECONDARY;
 app.listen(port,()=>console.log(`web service running on port ${port}`));
