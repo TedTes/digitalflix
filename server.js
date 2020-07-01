0fa8d7e1f1ed817dc21ea9b9e599ca3ddf7a8059
Tedros <tedtfu@gmail.com>
2020-07-01 09:59:07 -0400

    server directory change

---
+++ server.js
@@ -0,0 +1,42 @@
+const path =require('path');
+require('dotenv').config({path:'../ecom_config.env'});
+const express=require('express');
+const {connectToDB,insertOrders,register,login,ordersList}=require('./Database.js')
+const cors=require('cors');
+const app=express();
+
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+
+// app.use('/api',(req,res,next)=>router(req,res,next));
+
+( function(){
+ connectToDB();
+})();
+app.get('/',(req,res)=>{
+    console.log("home page");
+})
+app.post('/',(req,res)=>{
+    // console.log("from post")
+    // console.log(req.body);
+})
+app.post('/api/orders',(req,res)=>{
+    insertOrders(req.body)
+})
+app.post('/api/register',async (req,res)=>{
+ res.send(await register(req.body)) ;
+})
+app.get('/api/orderslist',async (req,res)=>{
+    res.send(await ordersList());
+})
+
+app.post("/api/login",async (req,res)=>{
+res.send(await login(req.body));
+    });
+
+const port=3000 || process.env.PORT;
+app.listen(port,()=>console.log(`web service running on port ${port}`));
